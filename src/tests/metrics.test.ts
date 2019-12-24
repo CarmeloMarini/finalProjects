b3e5d9d731359ce0d5fbb33f1a476491969bb125
CarmeloMrn <camille.marini@edu.ece.fr>
2019-12-24 23:41:34 +0100

    lastCOMMIT

--- src/tests/metrics.test.ts
+++ src/tests/metrics.test.ts
@@ -25,53 +25,34 @@ describe('Metrics', function () {
       })
     })
   })
-  /*describe('#getOne', function () {
-    it('should get empty array on non existing group', function () {
-      dbMet.getOne( function (err: Error | null, result?: Metric[]) {
-        expect(err).to.be.null
-        expect(result).to.not.be.undefined
-        expect(result).to.be.empty
-      })
-    })
-  })*/
 
   after(function () {
     dbMet.db.close()
   })
-
-  
 })
 
-describe('Save', function () {
-  var metrics = new Array()
-
+describe("User", function() {   
   before(function () {
     LevelDB.clear(dbPath)
-    
-    dbMet = new MetricsHandler(dbPath)
-    var met1 : Metric
-    var met2 : Metric
-    var met3 : Metric
-    met1 = new Metric("Alfred", "testAlfred1", 1),
-    met2 =new Metric("Ming", "testMing1", 1),
-    met3 =new Metric("Edgar", "testEdgar1", 1),
-    metrics.push(met1)
-    metrics.push(met2)
-    metrics.push(met3)
-  })
-
-  /*describe('#save', function () {
-    it('test Save', function (done) {
-      dbMet.save(metrics, function (error: Error | null) {  
-        expect(error).to.be.undefined
-      })
-    })
-  })*/
-
-  after(function () {
-    dbMet.db.close()
-  })
+    dbUser = new UserHandler(dbPath)
+  })               
+  describe("#save", function() {
+      it("should save without error", function(done) {
+          const user = new User("Bob", "bob@gmail.com", "1234");
+          dbUser.save(user, (err) => {
+              if (err) done(err);
+              else done();
+          });
+      });
+      it("should return an error", function(done) {
+        dbUser.get("SERGAI", (err) => {
+            if (err) done();
+            else done(err);
+        });
+    });
+  });
+});
   
-})
+
 
 
