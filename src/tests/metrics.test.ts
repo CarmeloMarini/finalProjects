476b6dd55f2e9e324794203589efb7d3d2597938
CarmeloMrn <camille.marini@edu.ece.fr>
2019-12-24 22:39:53 +0100

    blabla

--- src/tests/metrics.test.ts
+++ src/tests/metrics.test.ts
@@ -75,25 +75,3 @@ describe('Save', function () {
 })
 
 
-describe('Users', function () {
-  before(function () {
-    LevelDB.clear(dbPath)
-    dbUser = new UserHandler(dbPath)
-  })
-
-  describe('#getAll', function () {
-    it('should get empty array on non existing group', function () {
-      dbUser.getAll(function (err: Error | null, result?: User[]) {
-        expect(err).to.be.null
-        expect(result).to.not.be.undefined
-        expect(result).to.be.empty
-      })
-    })
-  })
-
-  after(function () {
-    dbUser.db.close()
-  })
-
-  
-})
