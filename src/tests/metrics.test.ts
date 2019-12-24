19fcdb6e48dd657ed67b4fff544f2d185b23c408
CarmeloMrn <camille.marini@edu.ece.fr>
2019-12-24 22:38:27 +0100

    testCORRECTION

--- src/tests/metrics.test.ts
+++ src/tests/metrics.test.ts
@@ -1,11 +1,12 @@
 import 'chai'
 import { expect } from 'chai'
 import { Metric, MetricsHandler } from '../metrics'
+import { User, UserHandler } from '../user'
 import { LevelDB } from "../leveldb"
 
 const dbPath: string = 'db_test'
 var dbMet: MetricsHandler
-
+var dbUser: UserHandler
 
 
 
@@ -69,6 +70,29 @@ describe('Save', function () {
 
   after(function () {
     dbMet.db.close()
+  })
+  
+})
+
+
+describe('Users', function () {
+  before(function () {
+    LevelDB.clear(dbPath)
+    dbUser = new UserHandler(dbPath)
+  })
+
+  describe('#getAll', function () {
+    it('should get empty array on non existing group', function () {
+      dbUser.getAll(function (err: Error | null, result?: User[]) {
+        expect(err).to.be.null
+        expect(result).to.not.be.undefined
+        expect(result).to.be.empty
+      })
+    })
+  })
+
+  after(function () {
+    dbUser.db.close()
   })
 
   
